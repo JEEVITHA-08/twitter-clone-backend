3d85c54c6f4897a0dfe86ef9d4b9d5a4dc0b39ec
Your Name <jeevitha8052007@gmail.com>
2026-06-21 18:29:11 +0530

    Fix AuthController

--- src/main/java/com/jeevitha/twitter_clone/controller/AuthController.java
+++ src/main/java/com/jeevitha/twitter_clone/controller/AuthController.java
@@ -1,11 +1,11 @@
 package com.jeevitha.twitter_clone.controller;
 
+import com.jeevitha.twitter_clone.dto.LoginRequest;
+import com.jeevitha.twitter_clone.dto.RegisterRequest;
 import com.jeevitha.twitter_clone.entity.User;
 import com.jeevitha.twitter_clone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.jeevitha.twitter_clone.dto.LoginRequest;
-import com.jeevitha.twitter_clone.dto.RegisterRequest;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,22 +15,16 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return userService.registerUser(user);
-
-
-    }
-    @PostMapping("/login")
-    public String loginUser(@RequestBody LoginRequest loginRequest) {
-
-        return userService.loginUser(loginRequest);
-    }
-    @PostMapping("/register")
     public User register(@RequestBody RegisterRequest request) {
         User user = new User();
         user.setUsername(request.getUsername());
         user.setEmail(request.getEmail());
         user.setPassword(request.getPassword());
         return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
     }
 }
