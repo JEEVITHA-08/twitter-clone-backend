b767b12ed22cf659ef376639c39cdf86b90568bb
Your Name <jeevitha8052007@gmail.com>
2026-06-21 18:25:02 +0530

    Use RegisterRequest DTO for register endpoint

--- src/main/java/com/jeevitha/twitter_clone/controller/AuthController.java
+++ src/main/java/com/jeevitha/twitter_clone/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.jeevitha.twitter_clone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.jeevitha.twitter_clone.dto.LoginRequest;
+import com.jeevitha.twitter_clone.dto.RegisterRequest;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -23,5 +24,13 @@ public class AuthController {
     public String loginUser(@RequestBody LoginRequest loginRequest) {
 
         return userService.loginUser(loginRequest);
+    }
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        return userService.registerUser(user);
     }
 }
